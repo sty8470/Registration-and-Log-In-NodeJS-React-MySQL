e8732b33861def3239a4677b95dce1b20af9d54f
Taeyoung <sty8470@gmail.com>
2021-04-19 16:50:52 +0900

    Adding Hasing, Cookies, and Sessions
    In this version, I added the following functionality.
    
    1. Password hashing
    2. Creating cookies and sessions for the User Login

---
+++ client/src/App.js
@@ -0,0 +1,97 @@
+import React, {useState, useEffect} from "react";
+import Axios from 'axios';
+import './App.css';
+function App() {
+
+  //we need to grab the information of inputs and to validate them
+  //usernameReg = username that is registered
+  const [usernameReg, setUsernameReg] = useState('');
+  const [passwordReg, setPasswordReg] = useState('');
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loginStatus, setLoginStatus] = useState('');
+
+  //send information from frontend to the backend to see if session exists
+  Axios.defaults.withCredentials = true;
+
+  const register = () => {
+    Axios.post("http://localhost:3001/register", {
+      username: usernameReg, 
+      password: passwordReg,
+    
+    }).then((response) => {
+      console.log(response);
+    })
+    
+  };
+
+  const login = () => {
+    Axios.post("http://localhost:3001/login", {
+      username: username, 
+      password: password,
+    
+    }).then((response) => {
+      if (response.data.message) {
+        setLoginStatus(response.data.message);
+      } else{
+        setLoginStatus(response.data[0].username);
+      }
+      
+    })
+    
+  };
+
+  //every time we refresh our page, we want to get information from this URL if we are logged in or not.
+  useEffect(() => {
+   Axios.get("http://localhost:3001/login").then((response)=> {
+     if (response.data.loggedIn === true) {
+        setLoginStatus(response.data.user[0].username);
+        console.log(response);
+     }
+      
+   })
+  }, [])
+
+  return (
+    <div className="App">
+      <div className="registration">
+        <h1>Registration</h1>
+        <label>Username</label>
+        <input 
+          type="text" 
+          onChange={(e) => 
+            {setUsernameReg(e.target.value)}} 
+            />
+        <label>Password</label>
+        <input 
+          type="text" 
+          onChange={(e) => 
+            {setPasswordReg(e.target.value)}} 
+            />
+        <button onClick={register}> Register </button>
+      </div>
+
+
+      <div className="login">
+        <h1>Login</h1>
+        <input 
+          type="text" 
+          placeholder="Username..." 
+          onChange={(e) => 
+            {setUsername(e.target.value)}} 
+          />
+        <input type="password" 
+          placeholder="Password..." 
+          onChange={(e) => 
+            {setPassword(e.target.value)}} 
+          />
+        <button onClick={login}> Login </button>
+      </div>
+
+      <h1>{loginStatus}</h1>
+    </div>
+  );
+}
+export default App;
